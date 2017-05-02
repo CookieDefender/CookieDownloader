5c72b208ae9dcc1f23cc874acc89b678c9aa19d1
CookieDefender <nickthegreat4815@gmail.com>
2017-05-01 22:28:16 -0500

    directory for URL TO OGG, updated YouTube-DL

--- Youtube-DL/URL to OGG.bat
+++ Youtube-DL/URL to OGG.bat
@@ -1,10 +1,14 @@
 @echo off
 :start
-set /p dl="Enter URL: "
+set dl=
+set download=
 
+
+set /p dl="Enter URL: "
+set downloaddir=-o "Youtube Downloads/%%(title)s.%%(ext)s"
 
 :final
-"youtube-dl.exe" %dl% -x --extract-audio --audio-format "vorbis"
+"youtube-dl.exe" %dl% -x --extract-audio --audio-format "vorbis" %downloaddir%
 echo All done!
 pause
 goto:start
